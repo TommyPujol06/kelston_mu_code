d01a367bca7c1486d233b18a0bdcc9cbe282f9c8
Tim Golden <mail@timgolden.me.uk>
2018-11-18 17:07:32 +0000

    Create get_age.py
    Sample code for determining how old someone is from their year, month & day of birth

---
+++ get_age.py
@@ -0,0 +1,15 @@
+import datetime
+import calendar
+
+year = int(input("What year were you born? "))
+month = int(input("What month were you born? "))
+day = int(input("What day were you born? "))
+
+today = datetime.date.today()
+age = today.year - year
+if month > today.month:
+    age -= 1
+elif month == today.month and day > today.day:
+    age -= 1
+
+print("You are", age, "years old")
