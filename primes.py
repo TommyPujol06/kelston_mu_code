1c7e0c30a2ec8ead5c8d0b1f9e17dd7dc2ccb0dc
Tim Golden <mail@timgolden.me.uk>
2018-11-24 17:41:28 +0000

    Add a little documentation

--- primes.py
+++ primes.py
@@ -1,7 +1,19 @@
 def is_prime(n):
+    #
+    # Check every number up to half of the number
+    # we're checking since if we're over half way
+    # we must have hit all the factors already
+    #
     for factor in range(2, 1 + (n // 2)):
         if n % factor == 0:
+            #
+            # Bail out as soon as we've found a factor
+            #
             return False
+    
+    #
+    # If we haven't found anything, it's a prime
+    #
     return True
 
 while True:
