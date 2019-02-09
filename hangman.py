7fab58eb08dde51bc7db9aba66cdf9763c35c679
Linguini2004 <enrmas12@gmail.com>
2019-02-09 16:28:47 +0000

    fixing typos

--- hangman.py
+++ hangman.py
@@ -1,4 +1,6 @@
-import hangman_screen
+import os, sys
+sys.path.append(os.path.dirname(__file__))
+import hangman_screen     
 import hangman_words
 alive = True
 word_so_far = []
@@ -10,24 +12,23 @@ correct = False
 word_list = hangman_words.read_possible_words()
 
 # Ask the user for a difficulty level (length of word)
-difficulty = hangman_screen.get_difficulty()
-assert isinstance(difficulty, int)
-
+difficulty = hangman_screen.get_difficuty()
+    
 # Pick a random one(giving Tommy values)
-random_word = hangman_words.pick_a_word(word_list, difficulty)
+random_word = hangman_words.pick_a_word(word_list, int(difficulty))
 
 # Loop until you win or you lose
 word_so_far = ["_"] * difficulty
 while alive == True:
     #   Draw the current position
-    hangman_screen.draw(words_so_far, failed_letters)
+    current_pos = hangman_screen.draw(words_so_far, failed_letters)
     #   Ask for a letter
     letter = hangman_screen.get_a_letter()
     #   If the letter is good
     if hangman_words.letter_is_good(letter, random_word):
         #      Update the word-so-far
         word_so_far = hangman_words.update_word_so_far(random_word, word_so_far, letter)
-    else
+    else:
         #      Update the failed-letters
         failed_letters = hangman_words.update_failed_letters(failed_letters, letter)
 #   If the word-so-far is complete
